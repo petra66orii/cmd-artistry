727a22b201f4469e7141ae143ca300bbe72cac16
Petra <petra66orii@gmail.com>
2025-10-14 16:27:41 +0100

    Create gallery, #2

---
+++ cmd-artistry/src/pages/Gallery.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const GalleryPage: React.FC = () => {
+  return (
+    <div className="container mx-auto p-8">
+      <h1 className="text-4xl font-bold">Gallery</h1>
+      <p>This is where the photo gallery will be.</p>
+    </div>
+  );
+};
+
+export default GalleryPage;
