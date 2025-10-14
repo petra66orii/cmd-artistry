b91b1e144b906244e51608bdf1c955fafd0c6374
Petra <petra66orii@gmail.com>
2025-10-14 16:26:35 +0100

    Create services page, #3

---
+++ cmd-artistry/src/pages/Services.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ServicesPage: React.FC = () => {
+  return (
+    <div className="container mx-auto p-8">
+      <h1 className="text-4xl font-bold">Our Services</h1>
+      <p>This page will detail all of the services offered.</p>
+    </div>
+  );
+};
+
+export default ServicesPage;
