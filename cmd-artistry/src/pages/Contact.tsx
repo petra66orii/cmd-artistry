857d6669cc24430d8e08973a5567cdb31be3220b
Petra <petra66orii@gmail.com>
2025-10-14 16:27:50 +0100

    Create contact page, #1

---
+++ cmd-artistry/src/pages/Contact.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ContactPage: React.FC = () => {
+  return (
+    <div className="container mx-auto p-8">
+      <h1 className="text-4xl font-bold">Contact</h1>
+      <p>The contact form will be on this page.</p>
+    </div>
+  );
+};
+
+export default ContactPage;
